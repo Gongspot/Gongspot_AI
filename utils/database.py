f32460e579b79444df92151779dc42aa0aa05100
Yoonji <estelle0329@ewha.ac.kr>
2025-11-05 19:55:12 +0900

    feat: rollback 관련 내용 추가

--- utils/database.py
+++ utils/database.py
@@ -16,10 +16,15 @@ DB_NAME = os.getenv("DB_NAME")
 
 SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=3600,
+    future=True
+)
 
 # 데이터베이스 세션 관리
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
 
 # ORM 모델 기본 클래스
 Base = declarative_base()
@@ -29,5 +34,9 @@ def get_db():
     db = SessionLocal()
     try:
         yield db
+        db.commit()
+    except Exception:
+        db.rollback()
+        raise
     finally:
         db.close()
